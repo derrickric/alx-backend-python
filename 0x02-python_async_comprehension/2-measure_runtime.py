56b77d16d9220f9527185c9092e3376d31c520e2
derrickric <derrickmbaka@gmail.com>
2023-05-04 07:54:39 +0000

    async

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-"""Module: 2-measure_runtime"""
-import asyncio
+"""Module: 2-measure runtime"""
+from asyncio import gather
 from time import perf_counter
-from typing import List
+
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Coroutine that measures total runtime of async_comprehension"""
-    start_time: float = perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end_time: float = perf_counter()
-    return end_time - start_time
-
+    """returns exextio time"""
+    s = perf_counter()
+    await gather(*(async_comprehension() for _ in range(4)))
+    return perf_counter() - s
